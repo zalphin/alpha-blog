685e4005aac7a0b65f312dda68e0967fa16e1648
eric <sonic009@hotmail.com>
2020-02-20 18:03:47 +0000

    Added a create article page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 	
 	root 'pages#home'
 	get 'about', to: 'pages#about'
+	
+	resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
