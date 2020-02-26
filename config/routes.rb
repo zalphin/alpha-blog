a9481f879596c784f1eb08a7daa44510faaa09d3
eric <sonic009@hotmail.com>
2020-02-26 16:13:29 +0000

    Added more integration tests and created category index page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 	get 'signup', to: 'users#new'
 	resources :users, except: [:new]
 	
+	resources :categories, except: [:destroy]
+	
 	get 'login', to: 'sessions#new'
 	post 'login', to: 'sessions#create'
 	delete 'logout', to: 'sessions#destroy'
---
+++ test/controllers/categories_controller_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class CategoriesControllerTest < ActionDispatch::IntegrationTest
+	def index
+		@categories = Category.all
+	end
+	
+	def setup
+		@category = Category.create(name: 'sports')
+	end
+	
+	test "should get categories index" do
+		
+		get categories_path
+		assert_response :success
+	end
+	
+	test "should get new" do
+		get new_category_path
+		assert_response :success
+	end
+	
+	test "should get show" do
+		get category_path(@category)
+		assert_response :success
+	end
+	
+end
