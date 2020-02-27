c851e13a877137cf4647e0ca7e51352d5cea060a
eric <sonic009@hotmail.com>
2020-02-27 01:37:05 +0000

    Started article test skeleton

--- test/integration/create_article_test.rb
+++ test/integration/create_article_test.rb
@@ -1,1 +1,18 @@
-this is a test
+require 'test_helper'
+
+class CreateArticleTest < ActionDispatch::IntegrationTest
+	
+	def setup
+		@user = User.create(username: "testme", email: "good@email.com", password: "test1234", admin: true)
+	end
+	
+	test "get new article form and create article" do
+		
+	end
+	
+	test "invalid article name submission results in failure" do
+		
+	end
+
+
+end
