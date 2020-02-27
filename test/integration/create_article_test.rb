42a60a24c36d66b210dcb253c41dc95aeafbcd2a
eric <sonic009@hotmail.com>
2020-02-27 16:23:08 +0000

    Added integration tests for article

--- test/integration/create_article_test.rb
+++ test/integration/create_article_test.rb
@@ -3,16 +3,26 @@ require 'test_helper'
 class CreateArticleTest < ActionDispatch::IntegrationTest
 	
 	def setup
-		@user = User.create(username: "testme", email: "good@email.com", password: "test1234", admin: true)
+		@user = User.create(username: "testme", email: "good@email.com", password: "test1234")
 	end
 	
 	test "get new article form and create article" do
-		
-	end
-	
-	test "invalid article name submission results in failure" do
-		
+		sign_in_as(@user, "test1234")
+		get new_article_path
+		assert_template 'articles/new'
+		assert_difference 'Article.count', 1 do
+			post articles_path, params: { article: { title: "test title", description: "Article description test"}}
+			follow_redirect!
+		end
+		assert_template 'articles/show'
+		assert_match "test title", response.body
 	end
 
+	test "error not logged in when getting new article form" do
+		get new_article_path
+		follow_redirect!
+		assert_template 'pages/home'
+		assert_select '#flash_danger'
+	end
 
 end
